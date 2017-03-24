91550818a42944389415f2cf55b39c2e82d2ca11
slothOn <zxctyzx@163.com>
2017-03-24 01:55:13 -0700

    repository

--- app/views.py
+++ app/views.py
@@ -3,14 +3,20 @@ __author__ = 'zxc'
 from app import app
 from flask import render_template, request
 from app.utils import xml2data
+import time
 
-@app.route('/')
+@app.route('/abc')
 def index():
     return render_template("index.html", text="Hello World")
 
-@app.route('/', methods=['POST', ])
+@app.route('/', methods=['POST', 'GET'])
 def parsePost():
     content = request.data
-    xmldict = xml2data.parseResp(content)
+    #xmldict = xml2data.parseResp(content)
     #$def with (toUser,fromUser,createTime,content)
-    return render_template("txt_resp_template.xml", toUser=xmldict['fromUser'], fromUser=xmldict['toUser'],content=xmldict['content'])
+    xmldict = {}
+    xmldict['fromUser'] = 'abc'
+    xmldict['toUser'] = 'def'
+    xmldict['content'] = 'abcdef'
+    print(xmldict['fromUser'])
+    return render_template("txt_resp_template.xml", toUser=xmldict['fromUser'], fromUser=xmldict['toUser'],content=xmldict['content'], createTime=int(time.time()))
